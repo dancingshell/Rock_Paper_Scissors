5556c7b94f84c95511147820a47f8ee10109c49d
dancingshell <kaitlinjaffe@gmail.com>
2014-07-11 15:22:41 -0700

    Adds functionaility in JS for centering RPS on click- workinggit add -A

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
  function changeRock() {
-
   if (document.getElementById("clickrock").src = "image_source/rock.jpg") 
   {
-      document.getElementById("clickpaper").style.display = "none";
-      document.getElementById("clickscissors").style.display = "none";
-      document.getElementById("paper").style.border = 0;
-      document.getElementById("scissors").style.border = 0;
+      document.getElementById("paper").style.display = "none";
+      document.getElementById("scissors").style.display = "none";
+      document.getElementById("rock").style.margin="0 auto";
   }
 }
 
 function changePaper() {
   if (document.getElementById("clickpaper").src = "image_source/paper.png") 
   {
-      document.getElementById("clickrock").style.display = "none";
-      document.getElementById("clickscissors").style.display = "none";
-      document.getElementById("rock").style.border = 0;
-      document.getElementById("scissors").style.border = 0;
+      document.getElementById("rock").style.display = "none";
+      document.getElementById("scissors").style.display = "none";
+      document.getElementById("paper").style.margin="0 auto";
   }
 }
 
 function changeScissors() {
   if (document.getElementById("clickscissors").src = "image_source/scissors.png") 
   {
-      document.getElementById("clickrock").style.display = "none";
-      document.getElementById("clickpaper").style.display = "none";
-      document.getElementById("rock").style.border = 0;
-      document.getElementById("paper").style.border = 0;
+      document.getElementById("rock").style.display = "none";
+      document.getElementById("paper").style.display = "none";
+      document.getElementById("scissors").style.margin="0 auto";
   }
 }
+
+// var imgObj = null;
+// var animate ;
+// function init(){
+//    imgObj = document.getElementById('clickrock');
+//    imgObj.style.position= 'relative'; 
+//    imgObj.style.left = '0px'; 
+// }
+// function moveRight(){
+//    imgObj.style.left = parseInt(imgObj.style.left) + 100 + 'px';
+//    animate = setTimeout(moveRight,20); // call moveRight in 20msec
+// }
