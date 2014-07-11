18aa8b4315cc887ea0bad182774835d6dc5303d9
dancingshell <kaitlinjaffe@gmail.com>
2014-07-11 09:28:38 -0700

    Changes to java file and additions to CSS to attempt to make remnant border dissapear. Not yet successful.

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
  function changeImage() {
 
-  if (document.getElementById("clickrock").src == "http://www.userinterfaceicons.com/80x80/minimize.png") 
+  if (document.getElementById("clickrock").src = "image_source/rock.jpg") 
   {
-      document.getElementById("imgClickAndChange").src = "http://www.userinterfaceicons.com/80x80/maximize.png";
+      document.getElementById("clickpaper").src = "";
+      document.getElementById("clickscissors").src = "";
+      document.getElementById("paper").style.border = 0;
+      document.getElementById("scissors").style.border = 0
   }
-  else 
+  else if (document.getElementById("clickpaper").src = "image_source/paper.png") 
   {
-      document.getElementById("clickrock").src = "http://www.userinterfaceicons.com/80x80/minimize.png";
+      document.getElementById("clickrock").src = "";
+      document.getElementById("clickscissors").src = "";
+      document.getElementById("rock").style.border = 0;
+      document.getElementById("scissors").style.border = 0;
   }
+  else if (document.getElementById("clickscissors").src = "image_source/scissors.png") 
+  {
+      document.getElementById("clickrock").src = "";
+      document.getElementById("clickpaper").src = "";
+      document.getElementById("rock").style.border = 0;
+      document.getElementById("paper").style.border = 0;
+  }
+ 
 }
