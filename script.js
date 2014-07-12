8ce6eb78144b427b8f34a8781f171ca14d64b67b
dancingshell <kaitlinjaffe@gmail.com>
2014-07-11 19:45:58 -0700

    changes 33% chance to more statistically correct 100/3

--- script.js
+++ script.js
@@ -30,12 +30,15 @@ function changeImage(user_choice) {
 }
 
 function randomChoice(user_choice) {
+  var first_third = 100/3
+  var second_third = (100/3) * 2
+
   var random_number = Math.floor((Math.random() * 100));
-  if (random_number <= 33) 
+  if (random_number <= first_third) 
   {
     comp = "rock";
   }
-  else if (random_number > 33 && random_number <= 66)
+  else if (random_number > first_third && random_number <= second_third)
   {
     comp = "paper";
   }
@@ -49,55 +52,44 @@ function randomChoice(user_choice) {
 function game(user_choice, comp_choice) {
   document.getElementById("user_result").innerHTML="You chose " + user_choice + ".";
   document.getElementById("comp_result").innerHTML="The computer chose " + comp_choice + ".";
-  // alert("You chose " + user_choice + ".");
-  // alert("The computer chose " + comp_choice + ".");
 
   if (user_choice == "rock") {
 
     if (comp_choice == "rock") {
       document.getElementById("final_result").innerHTML="You TIED :/";
-      // alert("You TIED :/")
     }
     else if (comp_choice == "paper") {
       document.getElementById("final_result").innerHTML="You LOST :(";
-      // alert("You LOST :(")
     }
     else if (comp_choice == "scissors") {
       document.getElementById("final_result").innerHTML="You WON! :)";
-      // alert("You WON :)")
     }
   }
 
   else if (user_choice == "paper") {
     if (comp_choice == "rock") {
       document.getElementById("final_result").innerHTML="You WON! :)";
-      // alert("You WON :)")
     }
     else if (comp_choice == "paper") {
       document.getElementById("final_result").innerHTML="You TIED :/";
-      // alert("You TIED :/")
     }
     else if (comp_choice == "scissors"){
       document.getElementById("final_result").innerHTML="You LOST :(";
-      // alert("You LOST :(")
     }
   }
 
   else if (user_choice == "scissors") {
     if (comp_choice == "rock") {
       document.getElementById("final_result").innerHTML="You LOST :(";
-      // alert("You LOST :(")
     }
     else if (comp_choice == "paper") {
       document.getElementById("final_result").innerHTML="You WON! :)";
-      // alert("You WON :)")
     }
     else if (comp_choice == "scissors") {
       document.getElementById("final_result").innerHTML="You TIED :/";
-      // alert("You TIED :/")
     }
   }
-  document.getElementById("play_again").innerHTML="Want to play again?";
+  document.getElementById("play_again").innerHTML="Play Again";
 }
 
   function reset(){
