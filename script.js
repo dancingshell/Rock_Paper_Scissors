16d5766638b6a53e32edf03640171e9930a5738c
dancingshell <kaitlinjaffe@gmail.com>
2014-07-13 22:14:16 -0700

    attempt at making a scroll bar before images show up

--- script.js
+++ script.js
@@ -16,34 +16,76 @@ function randomChoice(user_choice) {
   {
     comp = "scissors";
   }
-  game_result(user_choice, comp);
+  window.setTimeout( game_result(user_choice, comp), 800);
 }
 
-function test(string){
-  document.getElementById("title").innerHTML="this is a " +string;
+
+//   var i = 0;
+//   for (var i = 0; i >= 6; i++) {
+//     function test(string){
+//      document.getElementById("title").innerHTML="this is a " +string;
+//    }
+// }
+
+// var string1= "squiggle";
+// var string2= "butt";
+// var string3= "  .  ";
+// var string4= "   . ";
+// var string5= "    .";
+// // var string6= "test";
+
+function dots(user_choice) { window.setInterval( function() {
+    var wait = document.getElementById("title");
+    var i = 0;
+      
+    do {
+   
+    if ( wait.innerHTML.length > 2 ) {
+        wait.innerHTML = "";
+        }
+    else {
+        wait.innerHTML += ".";
+        i++;}
+    } while (i < 1);
+    }, 1000);
 }
+// funtion time()
 
-var string1= "squiggle";
-var string2= "butt";
-var string3= "  .  ";
-var string4= "   . ";
-var string5= "    .";
-// var string6= "test";
-
-
-
+//   funtion time() {
+    
+//     wait.innerHTML = "";
+    
+//     window.setInterval( function() {
+//     while (i < 5 ) {
+//       wait.innerHTML += ".";
+//       i++;
+//     }
+//   }, 1000);}
 
 
 function game_result(user_choice, comp_choice) {
   document.getElementById("clickuser").style.display="initial";
   document.getElementById("clickcomp").style.display="initial";
-  setTimeout(test(string1), 3000);
-  setTimeout(test(string2), 3000);
-  setTimeout(test(string3), 3000);
-  setTimeout(test(string4), 3000);
-  setTimeout(test(string5), 3000);
+  // setInterval(test(string1), 3000);
+  // setInterval(test(string2), 3000);
+  // setTimeout(test(string3), 3000);
+  // setTimeout(test(string4), 3000);
+  // setTimeout(test(string5), 3000);
   // setTimeout(test(string6), 3000);
   // document.getElementById("title").innerHTML="And the winner is...";
+
+// document.getElementById("title").innerHTML = "";
+// var wait = document.getElementById("title");
+// var i = document.getElementById("title").innerHTML.length;
+// // function time () {
+//   window.setInterval( function() {
+//     while (i < 6 ) {
+//       wait.innerHTML += ".";
+//       i++;
+//     }
+//   }, 1000);
+
+
   document.getElementById("rock").style.display = "none";
   document.getElementById("paper").style.display = "none";
   document.getElementById("scissors").style.display = "none";
@@ -100,6 +142,14 @@ function game_result(user_choice, comp_choice) {
     }
   }
   document.getElementById("play_again").innerHTML="Play Again";
+}
+
+function border() {
+  document.getElementById("play_again").style.border="5px solid black"
+}
+
+function borderOff() {
+  document.getElementById("play_again").style.border="none"
 }
 
 function reset() {
