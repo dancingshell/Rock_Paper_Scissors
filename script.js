9a9fc82641ad8e8bb644a9135659ed09cd484517
dancingshell <kaitlinjaffe@gmail.com>
2014-07-13 20:45:14 -0700

    multiple updates in JS functionality and refracter JS code. smoother transitions during game result.

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-
-function changeImage(user_choice) {
-  
-  randomChoice(user_choice);
-}
-
 function randomChoice(user_choice) {
   var first_third = 100/3;
   var second_third = (100/3) * 2;
+  var comp;
 
   var random_number = Math.floor((Math.random() * 100));
   if (random_number <= first_third) 
@@ -24,67 +19,83 @@ function randomChoice(user_choice) {
   game_result(user_choice, comp);
 }
 
+function test(string){
+  document.getElementById("title").innerHTML="this is a " +string;
+}
+
+var string1= "squiggle";
+var string2= "butt";
+var string3= "  .  ";
+var string4= "   . ";
+var string5= "    .";
+// var string6= "test";
+
+
+
+
+
 function game_result(user_choice, comp_choice) {
-  document.getElementById("title").innerHTML="";
+  document.getElementById("clickuser").style.display="initial";
+  document.getElementById("clickcomp").style.display="initial";
+  setTimeout(test(string1), 3000);
+  setTimeout(test(string2), 3000);
+  setTimeout(test(string3), 3000);
+  setTimeout(test(string4), 3000);
+  setTimeout(test(string5), 3000);
+  // setTimeout(test(string6), 3000);
+  // document.getElementById("title").innerHTML="And the winner is...";
+  document.getElementById("rock").style.display = "none";
+  document.getElementById("paper").style.display = "none";
+  document.getElementById("scissors").style.display = "none";
   document.getElementById("user_result").innerHTML="You chose " + user_choice + ".";
   document.getElementById("comp_result").innerHTML="The computer chose " + comp_choice + ".";
+  
 
   if (user_choice == "rock") {
-    document.getElementById("rock").style.margin= "0 auto";
-
+    document.getElementById("clickuser").src = "image_source/rock.jpg";  
     if (comp_choice == "rock") {
-      document.getElementById("scissors").style.display = "none";
-      document.getElementById("clickpaper").src = "image_source/rock.jpg";
+      document.getElementById("clickcomp").src = "image_source/rock.jpg";
       document.getElementById("final_result").innerHTML="You TIED :/";
     }
     else if (comp_choice == "paper") {
-      document.getElementById("scissors").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/paper.png";
       document.getElementById("final_result").innerHTML="You LOST :(";
     }
     else if (comp_choice == "scissors") {
-      document.getElementById("paper").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/scissors.png";
       document.getElementById("final_result").innerHTML="You WON! :)";
     }
   }
 
   else if (user_choice == "paper") {
-    document.getElementById("paper").style.margin= "0 auto";
-
+    document.getElementById("clickuser").src = "image_source/paper.png";  
     if (comp_choice == "rock") {
-      document.getElementById("clickrock").src= "image_source/paper.png";
-      document.getElementById("clickpaper").src = "image_source/rock.jpg";
-      document.getElementById("scissors").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/rock.jpg";
       document.getElementById("final_result").innerHTML="You WON! :)";
     }
     else if (comp_choice == "paper") {
-      document.getElementById("scissors").style.display = "none";
-      document.getElementById("clickrock").src = "image_source/paper.png";
+      document.getElementById("clickcomp").src = "image_source/paper.png";
       document.getElementById("final_result").innerHTML="You TIED :/";
     }
     else if (comp_choice == "scissors"){
-      document.getElementById("rock").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/scissors.png";
       document.getElementById("final_result").innerHTML="You LOST :(";
     }
   }
 
   else if (user_choice == "scissors") {
-    document.getElementById("scissors").style.margin= "0 auto";
+    document.getElementById("clickuser").src = "image_source/scissors.png";  
 
     if (comp_choice == "rock") {
-      document.getElementById("clickrock").src = "image_source/scissors.png";
-      document.getElementById("clickscissors").src = "image_source/rock.jpg";
-      document.getElementById("paper").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/rock.jpg";
       document.getElementById("final_result").innerHTML="You LOST :(";
     }
     else if (comp_choice == "paper") {
-      document.getElementById("clickpaper").src = "image_source/scissors.png";
-      document.getElementById("clickscissors").src = "image_source/paper.png";
-      document.getElementById("rock").style.display = "none";
+      document.getElementById("clickcomp").src = "image_source/paper.png";
       document.getElementById("final_result").innerHTML="You WON! :)";
     }
     else if (comp_choice == "scissors") {
-      document.getElementById("rock").style.display = "none";
-      document.getElementById("clickpaper").src = "image_source/scissors.png";
+      document.getElementById("clickcomp").src = "image_source/scissors.png";
       document.getElementById("final_result").innerHTML="You TIED :/";
     }
   }
